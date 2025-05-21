6c8e3335d054721287a787dcb50f4412ccef26d6
pony40701 <pony40701@gmail.com>
2025-05-21 10:33:12 +0800

    05211033

--- hi1/src/main/java/tw/pony/utils/HibernateUtil.java
+++ hi1/src/main/java/tw/pony/utils/HibernateUtil.java
@@ -1,13 +1,24 @@
 package tw.pony.utils;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
 	private static SessionFactory sessionFactory;
+	private static StandardServiceRegistry registry;
 
 	public static SessionFactory getSessionFactory() {
-		return null;
+		if (sessionFactory == null) {
+			Configuration config = new Configuration();
+			config.configure("hibernate.cfg.xml");
+
+			registry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+			sessionFactory = config.buildSessionFactory(registry);
+		}
+		return sessionFactory;
 	}
 
 }
