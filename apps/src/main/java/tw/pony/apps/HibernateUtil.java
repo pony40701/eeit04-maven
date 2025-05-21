a26dd2375bd68476b9caa03f5329a94fa60a7d14
pony40701 <pony40701@gmail.com>
2025-05-21 10:21:41 +0800

    05211021

---
+++ apps/src/main/java/tw/pony/apps/HibernateUtil.java
@@ -0,0 +1,9 @@
+package tw.pony.apps;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+
+public class HibernateUtil {
+	private static StandardServiceRegistry registry;
+	private static SessionFactory sessionFactory;
+}
